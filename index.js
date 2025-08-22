d5afe2bce21c9fe257b77d4e3f0f9ce145a43d03
ariel0147 <arial055665@gmail.com>
2025-08-22 10:55:28 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+let list = [];
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+app.listen(port, () =>{console.log(`http://localhost:${port}`)})
+
+app.get('/L', (req, res) => {
+
+    res.json(list);
+});
+app.get('/L/:p', (req, res) => {
+  let list1 = req.params.p;
+    let obj = {list1};
+    list.push(obj);
+
+    res.json({message:"ok"});
+
+
+});
+app.listen(port,()=>{console.log(`http://localhost:${port}`)});
